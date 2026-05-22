8a688bb335d29f1bc9a7001a4e6b3ec252ef61e9
kaludivine2025-droid <kaludivine2025@gmail.com>
2026-05-22 09:13:41 +0100

    Add files via upload

---
+++ Tailwind CSS Assignment/CSS/Config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue': '#0f172a',
+        'blue-shade': '#152032',
+        'light-blue': '#3b82f6',
+        'purple': '#a855f7',
+        'green': '#22c55e',
+      }
+    },
+  },
+  plugins: [],
+}
